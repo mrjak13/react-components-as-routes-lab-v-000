4bc3098580afdf39d810a8b8f68a81dd1c8c7b14
Justin Kenney <justinkenney13@gmail.com>
2019-06-06 17:02:08 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+	        <div className="movie">
+	       		<h3>{movie.title}</h3>
+	       		<h4>{movie.time}</h4>
+	       		{movie.genres.map(genre => (
+					<ul>{genre}</ul>
+	       		))}
+	        </div>
+
+        ))}
     </div>
   );
 };
